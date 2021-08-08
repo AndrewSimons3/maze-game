b788e49dffb9134dadebaf942c7d520ecaf35c07
AndrewSimons3 <simdrew07@gmail.com>
2021-08-08 08:35:19 -0500

    updated logic for walls

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const { Engine, Render, Runner, World, Bodies, MouseConstraint, Mouse } = Matter;
+const { Engine, Render, Runner, World, Bodies } = Matter;
 
-const width = 800;
+const width = 600;
 const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
@@ -16,40 +17,17 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(world, MouseConstraint.create(engine, {
-  mouse: Mouse.create(render.canvas)
-}))
-
 // Walls
 const walls = [
-  Bodies.rectangle(400, 0, 800, 40, {isStatic: true}),
-  Bodies.rectangle(400, 600, 800, 40, {isStatic: true}),
-  Bodies.rectangle(0, 300, 40, 600, {isStatic: true}),
-  Bodies.rectangle(800, 300, 40, 600, {isStatic: true}),
+  Bodies.rectangle(width / 2, 0, width, 40, {isStatic: true}),
+  Bodies.rectangle(width / 2, height, width, 40, {isStatic: true}),
+  Bodies.rectangle(0, height / 2, 40, height, {isStatic: true}),
+  Bodies.rectangle(width, height / 2, 40, height, {isStatic: true}),
 ];
 World.add(world, walls);
 
 
-// Random Shapes
 
-
-for (let i = 0; i < 50; i++) {
-  if (Math.random() > 0.5) {
-    World.add(
-      world, 
-      Bodies.rectangle(Math.random() * width, Math.random() * height, 50, 50)
-    );
-  } else {
-    World.add(
-      world,
-      Bodies.circle(Math.random() * width, Math.random() * height, 35, {
-        render: {
-          fillStyle: 'green'
-        }
-      })
-    )
-  }
-};
 
 //BUILDING THE MAZE
 //CREATE A GRID OF CELLS 
